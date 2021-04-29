45317eeef4c4cff20cfb69aad45968842304a10c
Marik Decide <rasslabone@gmail.com>
2021-04-29 12:12:25 +0700

    draw crosses by clicking on the cell

---
+++ TicTacToe.Web/wwwroot/js/game.js
@@ -0,0 +1,14 @@
+﻿TicTacToe.Views.Game = {
+    Game: null,
+    Options: null,
+    Initialized: false,
+    Init: function(options) {
+        Game = this;
+        this.Options = options;
+        $('#gameForm .col-4').click(function () {
+            $(this).find('.empty-cell').addClass('hidden');
+            $(this).find('.cross-cell').removeClass('hidden');
+        });
+        this.Initialized = true;
+    }
+};
