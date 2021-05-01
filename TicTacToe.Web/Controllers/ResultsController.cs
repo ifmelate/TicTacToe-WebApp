6033b5aa4b22287203bfa869e61141e172926065
Marik Decide <rasslabone@gmail.com>
2021-05-01 12:04:28 +0700

    add forgotten files to previous commit

---
+++ TicTacToe.Web/Controllers/ResultsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TicTacToe.Services;
+
+namespace TicTacToe.Web.Controllers
+{
+    public class ResultsController : Controller
+    {
+        private readonly IResultsService _resultsService;
+
+        public ResultsController(IResultsService resultsService)
+        {
+            _resultsService = resultsService;
+        }
+        public IActionResult Index()
+        {
+            var results = _resultsService.GetResults();
+            return View(results);
+        }
+    }
+}
